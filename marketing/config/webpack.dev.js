983b7d1411e16529b6085edb20a3daa9de793f94
Ousikai <mtfallsVR@protonmail.com>
2021-10-20 19:51:14 -0400

    Delegating Shared Module Selection

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig  = require('./webpack.common')
+const packageJson = require('../package.json')
 
 const devConfig = {
   mode: 'development',
@@ -17,7 +18,9 @@ const devConfig = {
       filename: 'remoteEntry.js',
       exposes: {
         './MarketingApp': './src/bootstrap'
-      }
+      },
+      // shared: ['react', 'react-dom']
+      shared: packageJson.dependencies,
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html'
