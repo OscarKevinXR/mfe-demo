3aa979c984e72370d1b8bbd0af4f4abd66737fce
Ousikai <mtfallsVR@protonmail.com>
2021-10-20 19:35:35 -0400

    5 - Generic Integration

--- container/src/App.js
+++ container/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { mount } from 'marketing/MarketingApp'
-
-console.log(mount)
+import MarketingApp from './components/MarketingApp'
 
 export default () => {
   return (
     <div>
       <h1>Hi There!</h1>
+      <hr />
+      <MarketingApp />
     </div>
   )
 }
