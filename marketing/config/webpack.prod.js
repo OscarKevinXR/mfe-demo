3201abe6cfa49812e83b6c3671c3076bd7551c37
Ousikai <mtfallsVR@protonmail.com>
2021-10-20 20:35:42 -0400

    6 - Production Webpack Config for Marketing

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -1,17 +1,23 @@
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react','@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime'],a
-          }
-        }
-      }
-    ]
-  }
+const { merge } = require('webpack-merge')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const commonConfig  = require('./webpack.common')
+const packageJson = require('../package.json')
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap'
+      },
+      shared: packageJson.dependencies
+    })
+  ]
 }
+
+module.exports = merge(commonConfig, prodConfig)
