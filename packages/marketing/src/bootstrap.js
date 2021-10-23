d46aad5dc92aaaacf3220f7a1310b59603e88806
Ousikai <mtfallsVR@protonmail.com>
2021-10-23 11:35:31 -0400

    add memory history to marketing app

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createMemoryHistory } from 'history'
 import App from './App'
 
 // Mount Function to Start App
 const mount = (el) => {
-  ReactDOM.render(<App/>, el)
+  const history = createMemoryHistory();
+  ReactDOM.render(<App history={history}/>, el)
 }
 
 // If we are in dev and in isolation, mount immediately
